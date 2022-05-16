3493e554381bbc5f1b246f2679bf0440b7c1dbac
Smartgreen-Makc <akuliygaz@gmail.com>
2022-05-16 15:24:34 +0300

    new

--- js/script.js
+++ js/script.js
@@ -26,6 +26,41 @@ const isMobile = {
 };
 if (isMobile.any()) {
 	document.body.classList.add('_touch');
+	let menuArrows = document.querySelectorAll('.menu__arrow');
+	if(menuArrows.length > 0){
+		for(let index = 0; index < menuArrows.length; index++){
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function(e){
+				menuArrow.parentElement.classList.toggle('_active');
+			})
+		}
+	}
 } else {
 	document.body.classList.add('_pc');
+}
+
+//Прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if(menuLinks.length > 0){
+	menuLinks.forEach(menuLink => {
+		menuLinks.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e){
+		const menuLink = e.target;
+		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior:"smooth"
+			});
+			e.preventDefault();
+
+		// 
+		}
+	}
+	
 }
