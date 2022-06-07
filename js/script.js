3e95948a31f4c0a720564f1e482c7c638ed8b96c
Smartgreen-Makc <akuliygaz@gmail.com>
2022-06-07 08:18:46 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,79 +1,21 @@
-"use srtuct"
 
-const isMobile = {
-	Android: function () {
-		return navigator.userAgent.match(/Android/i);
-	},
-	BlackBerry: function () {
-		return navigator.userAgent.match(/BlackBerry/i);
-	},
-	ioS: function () {
-		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	},
-	Opera: function () {
-		return navigator.userAgent.match(/Opera Mini/i);
-	},
-	Windows: function () {
-		return navigator.userAgent.match(/IEMobile/i);
-	},
-	any: function () {
-		return (isMobile.Android() ||
-			isMobile.BlackBerry() ||
-			isMobile.ioS() ||
-			isMobile.Opera() ||
-			isMobile.Windows());
-	}
-};
-if (isMobile.any()) {
-	document.body.classList.add('_touch');
-	let menuArrows = document.querySelectorAll('.menu__arrow');
-	if(menuArrows.length > 0){
-		for(let index = 0; index < menuArrows.length; index++){
-			const menuArrow = menuArrows[index];
-			menuArrow.addEventListener("click", function(e){
-				menuArrow.parentElement.classList.toggle('_active');
-			})
-		}
-	}
-} else {
-	document.body.classList.add('_pc');
+var controlsUp = document.querySelector('.contpols__up');
+var controlsDown = document.querySelector('.contpols__down');
+var controlsLeft = document.querySelector('.controls__left');
+var controlsRight = document.querySelector('.controls__right');
+var tank = document.querySelector('.tank');
+
+var tankPuha = document.querySelector('.tank__puha');
+
+if(controlsUp.classList.contains('.controls__up')){
+   tankPuha.style.cssText = `transform: rotate(10deg);top:90px;`;
 }
-//меню бургер
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
-if(iconMenu){
-	iconMenu.addEventListener("click", function (e) {
-		document.body.classList.toggle('_lock');
-		iconMenu.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
-	});
-	
-}
-//Прокрутка при клике
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if(menuLinks.length > 0){
-	menuLinks.forEach(menuLink => {
-		menuLink.addEventListener("click", onMenuLinkClick);
-	});
 
-	function onMenuLinkClick(e){
-		const menuLink = e.target;
-		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-			const gotoBlock = document.querySelector(menuLink.dataset.goto);
-			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+//tank.style.left = "200px";
 
-
-			if(iconMenu.classList.contains('_active')){
-				document.body.classList.remove('_lock');
-				iconMenu.classList.remove('_active');
-				menuBody.classList.remove('_active');
-			}
-
-			window.scrollTo({
-				top: gotoBlockValue,
-				behavior:"smooth"
-			});
-			e.preventDefault();
-		}
-	}
+//tankPuha.classList.add('active');
+//controlsUp.classList.add('active')
+console.log(tankPuha);
+for(let className of tankPuha.classList){
+   console.log(className);
 }
